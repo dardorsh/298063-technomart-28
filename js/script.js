25d5edd417182a41c8af0d73297fe3acbdd98861
dardorsh <65073890+dardorsh@users.noreply.github.com>
2020-06-23 13:22:58 +0300

    Правки к заданию "Оживляем интерактивные элементы"

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ var formLink = document.querySelector(".to-form-btn");
 var formPopup = document.querySelector(".form-popup");
 var formClose = formPopup.querySelector(".modal-close");
 var userName = formPopup.querySelector(".form-user-name");
-var formForm = formPopup.querySelector(".form-popup-form");
+var formSubmit = formPopup.querySelector(".form-popup-form");
 var userEmail = formPopup.querySelector(".form-user-email");
+
+var cartLink = document.querySelectorAll(".buy-button");
+var cartPopup = document.querySelector(".notification-popup");
+var cartClose = cartPopup.querySelector(".modal-close");
 
 var isStorageSupport = true;
 var storage = ""; 
@@ -32,9 +36,10 @@ formClose.addEventListener("click", function (evt) {
     formPopup.classList.remove("modal-error");
 });
 
-formForm.addEventListener("submit", function (evt) {
+formSubmit.addEventListener("submit", function (evt) {
     if (!userName.value || !userEmail.value) {
         evt.preventDefault();
+        formPopup.classList.remove("modal-error");
         formPopup.offsetWidth = formPopup.offsetWidth;
         formPopup.classList.add("modal-error");
     } else {
@@ -52,18 +57,14 @@ window.addEventListener ("keydown", function (evt) {
     }
 });
 
-var cartLink = document.querySelectorAll(".buy-button");
-var cartPopup = document.querySelector(".notification-popup");
-var cartClose = cartPopup.querySelector(".modal-close");
-
 for (var i=0; i < cartLink.length; i++) 
     cartLink[i].addEventListener("click", function(evt) {
         evt.preventDefault();
         cartPopup.classList.add("modal-show");
-    });
+        });
 
 
-cartPopup.addEventListener("click", function(evt) {
+cartClose.addEventListener("click", function(evt) {
     evt.preventDefault();
     cartPopup.classList.remove("modal-show");
 });
