c626586d79b9881bd86c7c29c062c71071b7213b
dardorsh <65073890+dardorsh@users.noreply.github.com>
2020-06-24 10:40:05 +0300

    Правки к заданию по JS

--- js/script.js
+++ js/script.js
@@ -1,80 +1,134 @@
 var formLink = document.querySelector(".to-form-btn");
 var formPopup = document.querySelector(".form-popup");
-var formClose = formPopup.querySelector(".modal-close");
-var userName = formPopup.querySelector(".form-user-name");
-var formSubmit = formPopup.querySelector(".form-popup-form");
-var userEmail = formPopup.querySelector(".form-user-email");
-
-var cartLink = document.querySelectorAll(".buy-button");
-var cartPopup = document.querySelector(".notification-popup");
-var cartClose = cartPopup.querySelector(".modal-close");
+var formClose = document.querySelectorAll(".modal-close");
+var userName = document.querySelector(".form-user-name");
+var formSubmit = document.querySelector(".form-popup-form");
+var userEmail = document.querySelector(".form-user-email");
 
 var isStorageSupport = true;
-var storage = ""; 
+var storage = "";
 
 try {
-    storage = localStorage.getItem("name");
+  storage = localStorage.getItem("name");
 } catch (err) {
-    isStorageSupport = false;
+  isStorageSupport = false;
 }
 
-formLink.addEventListener("click", function (evt) {
+if (formLink) {
+  formLink.addEventListener("click", function(evt) {
     evt.preventDefault();
     formPopup.classList.add("modal-show");
 
     if (storage) {
-        userName.value = storage;
-        userEmail.focus ();
+      userName.value = storage;
+      userEmail.focus();
     } else {
-        userName.focus();
+      userName.focus();
     }
-});
+  });
+}
 
-formClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    formPopup.classList.remove("modal-show");
-    formPopup.classList.remove("modal-error");
-});
+for (let j = 0; j < formClose.length; j++) {
+  formClose[j].addEventListener("click", function(evt) {
+    const modal = formClose[j].closest('.modal');
+    modal.classList.remove("modal-show");
+    modal.classList.remove("modal-error");
+  });
+}
 
-formSubmit.addEventListener("submit", function (evt) {
+if (formSubmit) {
+  formSubmit.addEventListener("submit", function(evt) {
     if (!userName.value || !userEmail.value) {
-        evt.preventDefault();
-        formPopup.classList.remove("modal-error");
-        formPopup.offsetWidth = formPopup.offsetWidth;
-        formPopup.classList.add("modal-error");
+      evt.preventDefault();
+      formPopup.classList.remove("modal-error");
+      formPopup.offsetWidth = formPopup.offsetWidth;
+      formPopup.classList.add("modal-error");
     } else {
-        localStorage.setItem("name", userName.value);
+      localStorage.setItem("name", userName.value);
     }
+  });
+}
+
+window.addEventListener("keydown", function(evt) {
+  var currentModal = document.querySelector(".modal.modal-show");
+  if (evt.keyCode === 27) {
+    if (currentModal) {
+      evt.preventDefault();
+      currentModal.classList.remove("modal-show");
+      currentModal.classList.remove("modal-error");
+    }
+  }
 });
 
-window.addEventListener ("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-        if (formPopup.classList.contains("modal-show")) {
-            evt.preventDefault();
-            formPopup.classList.remove("modal-show");
-            formPopup.classList.remove("modal-error");
-        }
-    }
-});
+// Cart popup
 
-for (var i=0; i < cartLink.length; i++) 
-    cartLink[i].addEventListener("click", function(evt) {
-        evt.preventDefault();
-        cartPopup.classList.add("modal-show");
-        });
+var cartLink = document.querySelectorAll(".buy-button");
+var cartPopup = document.querySelector(".notification-popup");
 
-
-cartClose.addEventListener("click", function(evt) {
+for (var i = 0; i < cartLink.length; i++) {
+  cartLink[i].addEventListener("click", function(evt) {
     evt.preventDefault();
-    cartPopup.classList.remove("modal-show");
-});
+    cartPopup.classList.add("modal-show");
+  });
+}
 
-window.addEventListener ("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-        if (cartPopup.classList.contains("modal-show")) {
-            evt.preventDefault();
-            cartPopup.classList.remove("modal-show");
-            cartPopup.classList.remove("modal-error");
-        }
-    }
-});
+// Map popup
+
+var mapLink = document.querySelector(".to-map-btn");
+var mapPopup = document.querySelector(".modal-map");
+
+if (mapLink) {
+  mapLink.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+  });
+}
+
+// Services slider
+
+var deliveryBtn = document.querySelector(".delivery-btn");
+var guaranteeBtn = document.querySelector(".guarantee-btn");
+var creditBtn = document.querySelector(".credit-btn");
+
+var delivery = document.querySelector(".delivery-item");
+var guarantee = document.querySelector(".quarantee-item");
+var credit = document.querySelector(".credit-item");
+
+if (deliveryBtn) {
+  deliveryBtn.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    delivery.classList.add("service-item-current");
+    guarantee.classList.remove("service-item-current");
+    credit.classList.remove("service-item-current");
+
+    deliveryBtn.classList.add("checked-btn");
+    guaranteeBtn.classList.remove("checked-btn");
+    creditBtn.classList.remove("checked-btn");
+  });
+}
+
+if (guaranteeBtn) {
+  guaranteeBtn.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    delivery.classList.remove("service-item-current");
+    guarantee.classList.add("service-item-current");
+    credit.classList.remove("service-item-current");
+
+    deliveryBtn.classList.remove("checked-btn");
+    guaranteeBtn.classList.add("checked-btn");
+    creditBtn.classList.remove("checked-btn");
+  });
+}
+
+if (creditBtn) {
+  creditBtn.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    delivery.classList.remove("service-item-current");
+    guarantee.classList.remove("service-item-current");
+    credit.classList.add("service-item-current");
+
+    deliveryBtn.classList.remove("checked-btn");
+    guaranteeBtn.classList.remove("checked-btn");
+    creditBtn.classList.add("checked-btn");
+  });
+}
