79edc834c8de985d804ffa737a97cdd365d45971
dardorsh <65073890+dardorsh@users.noreply.github.com>
2022-03-02 18:52:47 +0300

    console.log

--- jivosite/jivosite.js
+++ jivosite/jivosite.js
@@ -6,6 +6,7 @@
 let button = document.querySelector('.chat-btn');
 
 function jivo_onLoadCallback(){
+  console.log("Чат загружен полностью");
 	// Create a DIV element for the label
 
 	button.addEventListener('click', (e) => {
